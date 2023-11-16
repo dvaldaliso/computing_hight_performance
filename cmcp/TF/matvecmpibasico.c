3ef43163743829b01310bdbfc495e77eefee8a45
David Valdaliso Lopez <dvaldaliso@gmail.com>
2023-11-16 23:45:42 +0100

    work basci

--- cmcp/TF/matvecmpibasico.c
+++ cmcp/TF/matvecmpibasico.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#define NREPS 2
+#define NREPS 1000
 
 /* 
  * Multiplicación de una matriz banda por un vector
  *  w = A*v, con A matriz cuadrada de dimensión N y ancho de banda b
  *  Algoritmo orientado a filas
  */
+void imprimirVector(int rank, int size, double* wlocal, int b, int nlocal){
+   for (int i = 0; i < size; i++)
+    {
+        if (rank == i)
+        {
+            printf("rank %d: \n", rank);
+              for (int j = 0; j < nlocal + (2 * b); j++)
+            {
+               
+                    printf("%3.0f ", wlocal[j]);
+               
+            }
+            printf("\n");
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+}
 void matvec(int nlocal,int N,int b,double *A, double *vlocal, double *wlocal, int rank, int size)
 {
   int ilocal, jglobal, li, ls;
@@ -16,24 +33,11 @@ void matvec(int nlocal,int N,int b,double *A, double *vlocal, double *wlocal, in
   else prev = rank-1;
   if (rank == size-1) next = MPI_PROC_NULL;
   else next = rank+1;
-  //Prueba del send y recib
-  /*for (int i = 0; i < nlocal+2*b; i++)
-  {
-   printf(" v[%d]:%lf rank:%d ",i,vlocal[i],rank);
-  }
-  printf("\n");
-  MPI_Sendrecv( &vlocal[0],2, MPI_DOUBLE, next, 0, &vlocal[3], 2, MPI_DOUBLE, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
- */
-  
-// Envía al siguiente y recibe del anterior(abajo)  
-  MPI_Sendrecv( &vlocal[nlocal+b-1],b, MPI_DOUBLE, next, 0, &vlocal[0], b, MPI_DOUBLE, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //Envia al anterior y recibe del siguiente
-  MPI_Sendrecv( &vlocal[0],b, MPI_DOUBLE, prev, 0, &vlocal[nlocal+b-1], b, MPI_DOUBLE, next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  /*for (int i = 0; i < nlocal+2*b; i++)
-  {
-   printf(" v[%d]:%lf rank:%d ",i,vlocal[i],rank);
-  }*/
-  //printf(" rank:%d ",rank);
+ 
+  MPI_Sendrecv(&vlocal[b], b, MPI_DOUBLE, prev, 0, &vlocal[nlocal+b], b, MPI_DOUBLE, next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+  MPI_Sendrecv(&vlocal[nlocal], b, MPI_DOUBLE, next, 0, &vlocal[0], b, MPI_DOUBLE, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+ 
   
   for (ilocal=0; ilocal<nlocal; ilocal++) {
     int iglobal = ilocal +(nlocal*rank);
@@ -43,17 +47,14 @@ void matvec(int nlocal,int N,int b,double *A, double *vlocal, double *wlocal, in
     for (jglobal=li; jglobal<=ls; jglobal++) {
       int jlocal = jglobal - (nlocal*rank-b);
       wlocal[ilocal] += A[ilocal*N+jglobal]* vlocal[jlocal];
-      printf("V[%d]:%lf",jlocal, vlocal[jlocal]);
     }
   }
-  
-  printf("\n");
 }
 
 int main(int argc, char **argv) 
 {
   int i, j, k, N=50, b=4, nlocal;
-  double *A, *vlocal, *wlocal, *local_A;
+  double *A, *V,*vlocal, *wlocal, *local_A;
   MPI_Comm comm ;
 
 
@@ -83,6 +84,7 @@ int main(int argc, char **argv)
   /* Inicializar datos */
   if (rank == 0) {
       A = (double*)calloc(N * N , sizeof(double));
+      
       for (i=0; i<N; i++) A[i*N+i] = 2*b;
       for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
@@ -91,17 +93,33 @@ int main(int argc, char **argv)
           } 
         }
       }
+      V = (double*)calloc(N, sizeof(double));
+      for (k = 0; k < N; k++){
+         V[k] = 1.0;
+      }
   }
   
 // Scatter A and v
   //MPI_Scatter(sendbuf, sendcount, sendtype, recvbuf,recvcount, recvtype, root, comm)  
   MPI_Scatter( A, nlocal * N, MPI_DOUBLE, local_A, nlocal * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+ 
+  
+  //MPI_Scatter(v, local_rows, MPI_DOUBLE, localV + b, local_rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  //MPI_Scatter(V, nlocal , MPI_DOUBLE, &vlocal[b] , nlocal, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  for (i=b; i<nlocal+b; i++) vlocal[i] = 1.0; // inicia solo la parte local del vector
 
-  for (i=0; i<nlocal+2*b; i++) vlocal[i] = 1.0;
-
+  //double tInicio, tFinal;
+  //MPI_Barrier(MPI_COMM_WORLD);
+  // Inicio de medicion de tiempo
+  //tInicio = MPI_Wtime();  
 
   /* Multiplicación de matrices */
   for (k=0; k<NREPS; k++) matvec(nlocal, N, b, local_A, vlocal, wlocal, rank, size);
+  //MPI_Barrier(MPI_COMM_WORLD);
+  // fin de medicion de tiempo
+  //tFinal = MPI_Wtime();
+  //imprimirVector( rank,  size, wlocal, b, nlocal);
+ 
   double *W = NULL;
 
   if (rank == 0) {
@@ -114,6 +132,7 @@ int main(int argc, char **argv)
   /* Imprimir solución */
   printf("Matriz resultante W\n");
    if (rank == 0) {
+   //printf("Tiempo empleado en el cálculo: %g segundos", tFinal-tInicio);
    if (N<100) for (i=0; i<N; i++) printf("w[%d] = %g\n", i, W[i]);
       free(W);
   }
@@ -121,6 +140,7 @@ int main(int argc, char **argv)
   free(A);
   free(vlocal);
   free(wlocal);
+  MPI_Finalize();
 
   return 0;
 }
