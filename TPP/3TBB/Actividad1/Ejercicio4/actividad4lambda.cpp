d0448c9c31e6b49efc704e0190b93e178039a23b
David Valdaliso Lopez <dvaldaliso@gmail.com>
2023-12-13 20:06:55 +0100

    Ejercicio4 terminado

---
+++ TPP/3TBB/Actividad1/Ejercicio4/actividad4lambda.cpp
@@ -0,0 +1,36 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_reduce.h"
+#include <cstdlib>
+using namespace tbb;
+
+//g++ -o programa programa.cpp -ltbb
+double Foo( double f ){ 
+  unsigned int u=(unsigned int)f; 
+  double a = (double) rand_r(&u)/RAND_MAX; 
+  a = a*a; 
+  return a; 
+}
+
+double ParallelSumFoo(  double a[], size_t n ) {
+  const int GrainSize=1;
+ return parallel_reduce( blocked_range<size_t>(0, n, GrainSize), 
+  0.d,
+  [=](const blocked_range<size_t>& r, double sum)->double {
+    for( size_t i=r.begin(); i!=r.end(); ++i )
+        sum += Foo(a[i]);
+    return sum;  
+  },
+  [] (double x, double y)->double {return x+y;} 
+  );
+}
+int main( ) {
+  size_t n = 10000;
+  double a[n];
+  double sumas = ParallelSumFoo( a, n );
+  printf("Sumas = %f\n",sumas);
+}
+
+
