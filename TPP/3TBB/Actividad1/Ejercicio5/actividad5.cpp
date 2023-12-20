699ce88a41f28707f5d04313fdabc580c741639f
David Valdaliso Lopez <dvaldaliso@gmail.com>
2023-12-20 20:03:30 +0100

    ejercicio 5 de Tbb

---
+++ TPP/3TBB/Actividad1/Ejercicio5/actividad5.cpp
@@ -0,0 +1,73 @@
+
+#include <stdlib.h>
+#include <cfloat>
+#include <iostream>
+#include <tbb/parallel_reduce.h>
+#include <tbb/blocked_range.h>
+using namespace std;
+using namespace tbb;
+double Foo( double f ) { 
+  double a = f*f; 
+  return a;
+}
+class MinIndexFoo {
+	const double *const my_a;
+	public:
+		double value_of_min;
+		long index_of_min;
+		void operator()( const blocked_range<size_t>& r ) {
+			const double *a = my_a;
+				for( size_t i=r.begin(); i!=r.end(); ++i ) {
+					double value = Foo(a[i]);
+					if( value<value_of_min ) {
+					value_of_min = value;
+					index_of_min = i;
+					}
+				}
+		}
+		MinIndexFoo( const double a[] ) :
+		my_a(a),
+		value_of_min(FLT_MAX),
+		index_of_min(-1) { }
+		MinIndexFoo( MinIndexFoo& x, split ) :
+		my_a(x.my_a),
+		value_of_min(FLT_MAX),
+		index_of_min(-1){}
+		void join( const MinIndexFoo& y ) {
+			if( y.value_of_min<value_of_min ) {
+				value_of_min = y.value_of_min;
+				index_of_min = y.index_of_min;
+			}
+		}
+};
+
+/*long SerialMinIndexFoo( const double a[], size_t n ) {
+  double value_of_min = RAND_MAX; 
+  long index_of_min = -1; 
+  for( size_t i=0; i<n; ++i ) {
+    double value = Foo(a[i]); 
+    if( value < value_of_min ) {
+      value_of_min = value; 
+      index_of_min = i;
+    }
+  } 
+  return index_of_min;
+}
+*/
+
+long ParallelMinIndexFoo(const double a[], size_t n) {
+    MinIndexFoo mif(a);
+    parallel_reduce(blocked_range<size_t>(0,n), mif);
+    return mif.index_of_min;
+}
+
+int main( )  {
+
+  long int n = 1000000;
+  double *A = (double *) malloc( n*sizeof(double) );
+  for( size_t i=0; i<n; ++i ) A[i] = (double) rand() / RAND_MAX;
+  long indice = ParallelMinIndexFoo( A, n );
+  cout << "Minimo número = " << A[indice] << endl;
+
+}
+
