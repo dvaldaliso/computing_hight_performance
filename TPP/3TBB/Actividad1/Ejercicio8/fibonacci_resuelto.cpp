0c16df65afad454cc4c9a3348a94acfda2eac95e
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-09 18:39:29 +0100

    Ejercicio 8 y p de TBB

--- TPP/3TBB/Actividad1/Ejercicio8/fibonacci_resuelto.cpp
+++ TPP/3TBB/Actividad1/Ejercicio8/fibonacci_resuelto.cpp
@@ -17,16 +17,27 @@ size_t fib( size_t n ) {
 size_t fib1( size_t n ) {
   /* Implementad la version con parallel_invoke  */
   size_t i, j;
-  if (n<2) m = n;
-  else {
-    parallel_invoke( FibClass( n-1, i ),
-    FibClass( n-2, j ) );
-      m = i+j;
-    }
+  if (n<2) 
+     return n; 
+   else { 
+         parallel_invoke( [&]{i=fib(n-1);},  
+                          [&]{j=fib(n-2);} ); 
+         return i+j; 
+   } 
 }
 
 size_t fib2( size_t n ) {
   /* Implementad la version con task_group  */
+  size_t i, j; 
+   if (n<2) 
+     return n; 
+   else { 
+         task_group g;
+         g.run([&]{i=fib(n-1);}); 
+         g.run([&]{j=fib(n-2);}); 
+         g.wait();
+         return i+j; 
+   }
 }
 
 int main( int argc, char *argv[] ) {
