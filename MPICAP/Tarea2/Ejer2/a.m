93c26466cb748a392c0009714ffdd9c27a0b37da
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-14 02:55:53 +0100

    cmod hoseholder

--- MPICAP/Tarea2/Ejer2/a.m
+++ MPICAP/Tarea2/Ejer2/a.m
@@ -2,22 +2,31 @@ A=[3,1,2,6;4,5,6,3;1,8,1,2;5,9,5,5]
 [m,n] = size(A);
 
 for j = 1:n
-    ro = norm(A(:,j))
+    pj = norm(A(:, j))
+     v = A(:, j);
+     v(j,1) = pj + A(j, j);
+     t = transpose(v);
+     Bj = (t * v)/2;
     for k = j:n
-        yij = fact(j,k,A,ro,m)
+       [yjk]=fact(j, k, A, v, m, Bj)
+       cmod(j, k, yjk, A, v, m)
+       %cmod
     end    
 end
 
-function fact(j, k, A, ro,m)
+function [yjk] = fact(j, k, A, v, m, Bj)
     % Factor
-    v = A(:,j)
-    v(j,1)=ro+A(j,k)
-    t=v'
-    Bj = (v' * v)/2
-    yij=0
+    sum= 0;
     for i=j:m
-        tem = v(i) * A(i,k)
-        yij=yij + tem
+        tem = v(i) * A(i,k);
+        sum = sum + tem;
     end
-    %return yij/Bj
+   yjk = sum/Bj;
 end
+
+function cmod(j, k, yjk, A, v, m)
+    for i = j:m
+        A(i, k) = A(i, k) - yjk * v(i) 
+    end
+end
+
