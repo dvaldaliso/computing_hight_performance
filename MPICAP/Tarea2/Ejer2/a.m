e8dcc0e82b8c05bb7457866bd80346c5d1634325
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-14 15:37:53 +0100

    house holder, triangular inferior con valores 0

--- MPICAP/Tarea2/Ejer2/a.m
+++ MPICAP/Tarea2/Ejer2/a.m
@@ -2,21 +2,27 @@ A=[3,1,2,6;4,5,6,3;1,8,1,2;5,9,5,5]
 [m,n] = size(A);
 
 for j = 1:n
-    pj = norm(A(:, j))
-     v = A(:, j);
-     v(j,1) = pj + A(j, j);
+     col=A(:, j)
+     if j-1 ~= 0
+        pos=j-1 
+        col(1:pos) = 0
+     end
+     pj = norm(col)
+     v = col;
+     v(j,1) = pj + A(j, j)
      t = transpose(v);
      Bj = (t * v)/2;
     for k = j:n
-       [yjk]=fact(j, k, A, v, m, Bj)
-       cmod(j, k, yjk, A, v, m)
+       [yjk] = fact(j, k, A, v, m, Bj)
+       [a]=cmod(j, k, yjk, A, v, m)
+       A=a
        %cmod
     end    
 end
 
 function [yjk] = fact(j, k, A, v, m, Bj)
     % Factor
-    sum= 0;
+    sum = 0;
     for i=j:m
         tem = v(i) * A(i,k);
         sum = sum + tem;
@@ -24,9 +30,8 @@ function [yjk] = fact(j, k, A, v, m, Bj)
    yjk = sum/Bj;
 end
 
-function cmod(j, k, yjk, A, v, m)
+function [A] = cmod(j, k, yjk, A, v, m)
     for i = j:m
         A(i, k) = A(i, k) - yjk * v(i) 
     end
 end
-
