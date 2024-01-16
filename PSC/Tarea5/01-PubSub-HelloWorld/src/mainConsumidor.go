e205526893741f1ec873ab813ccc05af8b527035
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-16 15:58:19 +0100

    envia mensaje y responde cuando recibe

--- PSC/Tarea5/01-PubSub-HelloWorld/src/mainConsumidor.go
+++ PSC/Tarea5/01-PubSub-HelloWorld/src/mainConsumidor.go
@@ -10,39 +10,73 @@ package main
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
 import (
-  "fmt"
-  "context"
+	"context"
+	"fmt"
+	"reflect"
 
-  kafka "github.com/segmentio/kafka-go"
+	kafka "github.com/segmentio/kafka-go"
 )
 
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
+func readMessage(lector *kafka.Reader, w *kafka.Writer) {
+	msg, err := lector.ReadMessage(context.Background())
+	if err != nil {
+		fmt.Println(" *** ERROR *** ")
+		fmt.Println(err)
+	}
+
+	fmt.Println(" --> recibo: ")
+	fmt.Println(msg.Topic)
+	fmt.Println(string(msg.Key))
+	fmt.Println(string(msg.Value))
+
+	rt := reflect.TypeOf(msg.Key)
+	switch rt.Kind() {
+	case reflect.Slice:
+		fmt.Println("is a slice with element type", rt.Elem())
+	case reflect.Array:
+		responder(w, msg)
+		fmt.Println("is an array with element type", rt.Elem())
+	default:
+		fmt.Println("is something else entirely")
+	}
+}
+
+func responder(w *kafka.Writer, msg kafka.Message) {
+	fmt.Println("responder")
+	texto := fmt.Sprintf("Hello World! %d", msg.Key)
+	fmt.Printf("envio: %s\n", texto)
+	w.WriteMessages(
+		context.Background(),
+		kafka.Message{
+			Key:   []byte(msg.Key),
+			Value: []byte(texto),
+		},
+	)
+}
 func main() {
-  fmt.Println( "consumidor empieza\n" )
+	fmt.Println("consumidor empieza\n")
 
-  lector := kafka.NewReader( kafka.ReaderConfig {
-    Brokers: []string {"localhost:9092"},
-    Topic: "topicazo1",
-  })
+	lector := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "trabajos",
+	})
+	// Inicializar writer
+	w := &kafka.Writer{
+		Addr:  kafka.TCP("localhost:9092"),
+		Topic: "respuesta",
+	}
 
-  defer lector.Close()
+	defer lector.Close()
 
-  fmt.Println( " espero mensajes ... " )
-  for {
-    msg, err := lector.ReadMessage( context.Background() )
-    if err != nil {
-      fmt.Println( " *** ERROR *** " )
-      fmt.Println( err )
-    }
+	fmt.Println(" espero mensajes ... ")
+	for {
+		readMessage(lector, w)
 
-    fmt.Println( " --> recibo: " )
-    fmt.Println( msg.Topic )
-    fmt.Println( string( msg.Key ) )
-    fmt.Println( string( msg.Value ) )
-  } // for
+	} // for
 
-  fmt.Println( " FIN " )
+	fmt.Println(" FIN ")
 
 } // ()
 
--- PSC/Tarea5/01-PubSub-HelloWorld/src/mainProductor.go
+++ PSC/Tarea5/01-PubSub-HelloWorld/src/mainProductor.go
@@ -10,35 +10,72 @@ package main
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
 import (
-  "fmt"
-  "context"
+	"context"
+	"fmt"
+	"os"
 
-  kafka "github.com/segmentio/kafka-go"
+	kafka "github.com/segmentio/kafka-go"
 )
 
+var i int = 0
+
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
+func enviarMensage(w *kafka.Writer) {
+
+	texto := fmt.Sprintf("Hello World! %d", i)
+	fmt.Printf("envio: %s\n", texto)
+	w.WriteMessages(
+		context.Background(),
+		kafka.Message{
+			Key:   []byte("Clave A"),
+			Value: []byte(texto),
+		},
+	)
+	i++
+	if i == 10 {
+		w.Close()
+		os.Exit(0)
+	}
+}
+
+func recibiRespuesta(lector *kafka.Reader, w *kafka.Writer) {
+
+	for {
+		msg, err := lector.ReadMessage(context.Background())
+
+		if err != nil {
+			fmt.Println(" *** ERROR *** ")
+			fmt.Println(err)
+		}
+
+		fmt.Println(" --> recibo: ")
+		fmt.Println(msg.Topic)
+		fmt.Println(string(msg.Key))
+		fmt.Println(string(msg.Value))
+
+		enviarMensage(w)
+	}
+
+}
 func main() {
-  fmt.Print( "productor empieza\n" )
+	fmt.Print("productor empieza\n")
+	w := &kafka.Writer{
+		Addr:  kafka.TCP("localhost:9092"),
+		Topic: "trabajos",
+	}
 
-  w := &kafka.Writer{
-    Addr:  kafka.TCP("localhost:9092"),
-    Topic: "topicazo1",
-  }
+	lector := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "respuesta",
+	})
 
-  for i:=0; i<=10; i++ {
-    texto := fmt.Sprintf( "Hello World! %d", i )
-    fmt.Printf( "envio: %s\n", texto )
-    w.WriteMessages(
-      context.Background(),
-      kafka.Message{
-        Key:   []byte("Clave_A"),
-        Value: []byte(texto),
-      },
-    )
-  } // for
+	//for i:=0; i<=10; i++ {
+	enviarMensage(w)
+	//} // for
 
-  w.Close()
+	//respuesta
+	recibiRespuesta(lector, w)
 
 } // ()
 
