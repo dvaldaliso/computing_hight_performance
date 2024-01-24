6af99bc88c7ec43d82654d3565919d7546344f52
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-24 12:42:54 +0100

    MPICAP

--- MPICAP/Tarea1/Ejerc1/corridas.m
+++ MPICAP/Tarea1/Ejerc1/corridas.m
@@ -1,27 +1,47 @@
-A=rand(100,100);
-B=rand(100,100);
-%tomar tiempo
-matProdCol = @() prodMatrizColumna(A,B);
-elapsed_timeColumnaCien = timeit(matProdCol);
+tiemposTabla = table('Size', [0, 4], 'VariableTypes', {'double', 'double', 'double', 'double'}, 'VariableNames', {'Fila', 'Columna', 'TiempoFila', 'TiempoColumna'});
 
-matProdFila = @() prodMatrizFila(A,B)
-elapsed_timeFilaCien = timeit(matProdFila);
+tamaMatrices = [100, 200,400,600,800,1000];
 
-A=rand(500,500);
-B=rand(1000,1000);
+for i = 1:length(tamaMatrices)
+    % Generar una matriz de tamaño tamañosMatrices(i) x tamañosMatrices(i)
+    size = tamaMatrices(i);
+    
+    A=rand(size,size);
+    B=rand(size,size);
+    %tomar tiempo
+    
+    matProdCol = @() prodMatrizColumna(A,B);
+    timeColumna = timeit(matProdCol);
+    
+    matProdFila = @() prodMatrizFila(A,B)
+    timeFil = timeit(matProdFila);
 
-matProdColQuin = @() prodMatrizColumna(A,B);
-elapsed_timeColumnaQuin = timeit(matProdColQuin);
 
-matProdFilaQuin =  @() prodMatrizFila(A,B)
-elapsed_timeFilaQuin = timeit(matProdFilQuin);
+    % Agregar los tiempos medidos a la tabla
+    tiemposTabla = [tiemposTabla; table(tamaMatrices(i), tamaMatrices(i), timeFil, timeColumna, 'VariableNames', {'Fila', 'Columna', 'TiempoFila', 'TiempoColumna'})];
+end
 
 
-A=rand(1000,1000);
-B=rand(1000,1000);
 
-matProdColMil = @() prodMatrizColumna(A,B);
-elapsed_timeColumnaMil = timeit(matProdColMil);
 
-matProdFilaMil =  @() prodMatrizFila(A,B)
-elapsed_timeFilaMil = timeit(matProdFilaMil);
+
+
+tiemposTablaBloc = table('Size', [0, 3], 'VariableTypes', {'double', 'double', 'double',}, 'VariableNames', {'tamaBloque', 'tiempo', 'TamaMatriz'});
+
+TamaBloque = [10, 50,100,200,600,800,1000,1200,1400];
+
+for i = 1:length(TamaBloque)
+    % Generar una matriz de tamaño tamañosMatrices(i) x tamañosMatrices(i)
+    sizeBloc = TamaBloque(i);
+    
+    A=rand(2000,2000);
+    B=rand(2000,2000);
+    
+    matProdBloc= @() matrixMultiplyBloques(A,B,sizeBloc)
+    timeBlock1 = timeit(matProdBloc);
+
+
+    % Agregar los tiempos medidos a la tabla
+    tiemposTablaBloc = [tiemposTablaBloc; table(TamaBloque(i), timeBlock1, 2000, 'VariableNames', {'tamaBloque', 'tiempo', 'TamaMatriz'})];
+end
+
