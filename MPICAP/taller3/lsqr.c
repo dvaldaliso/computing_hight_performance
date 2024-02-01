ff7f8db3f839a02fc491f02820dab1b8eb648ebd
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-02-01 20:28:56 +0100

    taller 3

--- MPICAP/taller3/lsqr.c
+++ MPICAP/taller3/lsqr.c
@@ -12,63 +12,109 @@ static char help[] = "Uses lsqr to reconstruct a CT image using its projections 
 // ejecucion en Kaha
 int main(int argc, char **argv)
 {
+	//profe mnichipr@inf.upv.es
 	/*  A COMPLETAR: DECLARACION DE VARIABLES*/
+	Mat            A;               /* operator matrix */
+	Vec            b;               /* operator matrix */
+  	Vec            x;               /* operator matrix */
+  
+	PetscInt       dim_rows, dim_cols;
+  	char           filename[PETSC_MAX_PATH_LEN];
+  	char           filenameb[PETSC_MAX_PATH_LEN];
+  	char           filenameS[PETSC_MAX_PATH_LEN];
+  	PetscViewer    viewer;
 	PetscBool flg;
+	KSP ksp;
+
+  	PetscReal t1,t2;
 
 	/*  A COMPLETAR: Inicializar PETSc (PetscFunctionBeginUser; PetscInitialize) */
-
+	PetscFunctionBeginUser; // Begin user section
+	PetscInitialize(&argc, &argv, (char*)0, help); // Initialize PETSc
 	/*  A COMPLETAR: Medir tiempos
 		EJEMPLO:
+	*/	
+		
 	    PetscCall(PetscTime(&t1));
-   		...
-   		PetscCall(PetscTime(&t2));
-   		PetscCall(PetscPrintf(PETSC_COMM_WORLD," Elapsed Time: %f\n",t2-t1));*/
-
+   		
+   		
 	/*	A COMPLETAR: Leer Datos de entrada: rutas matriz A, vector b y vector x 
 		EJEMPLO:
+	*/	
 		PetscCall(PetscOptionsGetString(NULL,NULL,"-file",filename,sizeof(filename),&flg));
-	    PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option"); */
+	    PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option");
+		PetscCall(PetscOptionsGetString(NULL,NULL,"-fileb",filenameb,sizeof(filenameb),&flg));
+	    PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option");
+		PetscCall(PetscOptionsGetString(NULL,NULL,"-files",filenameS,sizeof(filenameS),&flg));
+	    PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option");
 
 
 	/*	A COMPLETAR: Leer los datos de los ficheros (matriz A, vector b)
 
 		EJEMPLO:
+	*/	
 		PetscCall(PetscViewerBinaryOpen(PETSC_COMM_WORLD,filename,FILE_MODE_READ,&viewer));
 		PetscCall(MatCreate(PETSC_COMM_WORLD,&A));
 		PetscCall(MatSetFromOptions(A));
 		PetscCall(MatLoad(A,viewer));
-		PetscCall(PetscViewerDestroy(&viewer));*/
+		PetscCall(PetscViewerDestroy(&viewer));
+
+		MatGetSize(A, &dim_rows, &dim_cols); 
+
+		PetscCall(PetscViewerBinaryOpen(PETSC_COMM_WORLD,filenameb,FILE_MODE_READ,&viewer));
+		PetscCall(VecCreate(PETSC_COMM_WORLD,&b));
+		PetscCall(VecSetSizes(b, PETSC_DECIDE, dim_rows));
+		PetscCall(VecLoad(b,viewer));
+		PetscCall(PetscViewerDestroy(&viewer));
+
 
 	/*	A COMPLETAR: Crear vector x (solucion) inicializado a 0
 		EJEMPLO:
-		PetscCall(VecCreate(PETSC_COMM_WORLD, &u));
-		PetscCall(VecSetSizes(u, PETSC_DECIDE, dim));
-		PetscCall(VecSetFromOptions(u));
-		PetscCall(VecSet...*/
+	*/	
+		PetscCall(VecCreate(PETSC_COMM_WORLD, &x));
+		PetscCall(VecSetSizes(x, PETSC_DECIDE, dim_cols));
+		PetscCall(VecSetFromOptions(x));
+		//PetscCall(VecSet
+		PetscReal initial_guess = 0.0;
+		PetscCall(VecSet(x, initial_guess));
+		
 
 		/*	A COMPLETAR: Configurar solver KSP sin precondicionador, tipo KSPLSQR, 
 		FUNCIONES:
-		KSPSetOperators
-		KSPGetPC(ksp, &pc)
-		PCSetType(pc, PCNONE)
-		KSPSetType
-		KSPSetFromOptions */
+		*/
+		PC pc;
+		KSPSetOperators(ksp, A, NULL);
+		KSPGetPC(ksp, &pc);
+		PCSetType(pc, PCNONE);
+		KSPSetType(ksp, KSPLSQR);
+		KSPSetFromOptions(ksp);
+		 
 
 		/*	A COMPLETAR: Ejecutar solver KSP 
 		FUNCIONES:
-		KSPSolve */
+		*/
+		KSPSolve(ksp, b, x); 
 		
 		/*	A COMPLETAR: Extraer datos de la solución del solver KSP 
 		FUNCIONES:
 		Buscar las funciones para extraer el motivo de parada y numero de iteraciones realizadas*/
+		KSPConvergedReason reason;
+		KSPGetConvergedReason(ksp, &reason); // Get the reason for convergence
+		PetscPrintf(PETSC_COMM_WORLD, "Convergence reason: %s\n", KSPConvergedReasons[reason]);
 
+		PetscInt iterations;
+		KSPGetIterationNumber(ksp, &iterations); // Get the number of iterations
+		PetscPrintf(PETSC_COMM_WORLD, "Number of iterations: %D\n", iterations);	
 		/*	A COMPLETAR: Almacenar la solucion en formato MATLAB
 		FUNCIONES:
-		PetscViewerASCIIOpen(...)
+		*/
+		PetscViewer fdout;
+		PetscViewerASCIIOpen(PETSC_COMM_WORLD, filenameS, &fdout);
 		PetscViewerPushFormat(fdout,PETSC_VIEWER_ASCII_MATLAB);
 		VecView(x, fdout);
 		PetscViewerPopFormat(fdout);
-		PetscViewerDestroy(&fdout);*/
+		PetscViewerDestroy(&fdout);
+
 
 		/*A COMPLETAR: Destruir matrices, vectores y finalizar entorno
 		FUNCIONES:
@@ -77,8 +123,15 @@ int main(int argc, char **argv)
 		KSPDestroy
 		PetscFinalize
 		*/
+		MatDestroy(&A); // Destroy matrix A
+		VecDestroy(&b); // Destroy vector b
+		VecDestroy(&x); // Destroy vector x
+		KSPDestroy(&ksp); // Destroy the solver context
 
 		/*A COMPLETAR: Mostrar datos de tiempos*/
+		PetscCall(PetscTime(&t2));
+   		PetscCall(PetscPrintf(PETSC_COMM_WORLD," Elapsed Time: %f\n",t2-t1));
+		PetscFinalize(); // Finalize the PETSc environment
 
 	return 0;
 }
--- MPICAP/taller3/svd.c
+++ MPICAP/taller3/svd.c
@@ -19,57 +19,105 @@ int main(int argc, char **argv)
 {
    /*  A COMPLETAR: DECLARACION DE VARIABLES*/
 	PetscBool flg;
-
+	PetscReal t1,t2;
+	Mat            A;               /* operator matrix */
+	Vec            b;               /* operator matrix */
+  	Vec            x;               /* operator matrix */
+	PetscInt       dim_rows, dim_cols;
+	PetscViewer    viewer;
+	char           filename[PETSC_MAX_PATH_LEN];
+  	char           filenameb[PETSC_MAX_PATH_LEN];
+  	char           filenameS[PETSC_MAX_PATH_LEN];
+	KSP ksp;
 	/*  A COMPLETAR: Inicializar PETSc (PetscFunctionBeginUser; PetscInitialize) */
-
+	PetscFunctionBeginUser; // Begin user section
+	PetscInitialize(&argc, &argv, (char*)0, help); 
 	/*  A COMPLETAR: Medir tiempos
 		EJEMPLO:
+	*/	
 	    PetscCall(PetscTime(&t1));
-   		...
-   		PetscCall(PetscTime(&t2));
-   		PetscCall(PetscPrintf(PETSC_COMM_WORLD," Elapsed Time: %f\n",t2-t1));*/
-
+   	
+   		
 	/*	A COMPLETAR: Leer Datos de entrada: rutas matriz A, vector b y vector x 
 		EJEMPLO:
-		PetscCall(PetscOptionsGetString(NULL,NULL,"-file",filename,sizeof(filename),&flg));
-	    PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option"); */
+		*/
+	PetscCall(PetscOptionsGetString(NULL,NULL,"-file",filename,sizeof(filename),&flg));
+	PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option");
+	PetscCall(PetscOptionsGetString(NULL,NULL,"-fileb",filenameb,sizeof(filenameb),&flg));
+	PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option");
+	PetscCall(PetscOptionsGetString(NULL,NULL,"-files",filenameS,sizeof(filenameS),&flg));
+	PetscCheck(flg,PETSC_COMM_WORLD,PETSC_ERR_USER_INPUT,"Must indicate a file name with the -file option");
 
 
 	/*	A COMPLETAR: Leer los datos de los ficheros (matriz A, vector b)
 
 		EJEMPLO:
+	*/	
 		PetscCall(PetscViewerBinaryOpen(PETSC_COMM_WORLD,filename,FILE_MODE_READ,&viewer));
 		PetscCall(MatCreate(PETSC_COMM_WORLD,&A));
 		PetscCall(MatSetFromOptions(A));
 		PetscCall(MatLoad(A,viewer));
-		PetscCall(PetscViewerDestroy(&viewer));*/
+		PetscCall(PetscViewerDestroy(&viewer));
+
+		MatGetSize(A, &dim_rows, &dim_cols); 
+
+		PetscCall(PetscViewerBinaryOpen(PETSC_COMM_WORLD,filenameb,FILE_MODE_READ,&viewer));
+		PetscCall(VecCreate(PETSC_COMM_WORLD,&b));
+		PetscCall(VecLoad(b,viewer));
+		PetscCall(PetscViewerDestroy(&viewer));
 
 	/*	A COMPLETAR: Crear vector x (solucion) inicializado a 0
 		EJEMPLO:
-		PetscCall(VecCreate(PETSC_COMM_WORLD, &u));
-		PetscCall(VecSetSizes(u, PETSC_DECIDE, dim));
-		PetscCall(VecSetFromOptions(u));
-		PetscCall(VecSet...*/
+	.*/	
+		PetscCall(VecCreate(PETSC_COMM_WORLD, &x));
+		PetscCall(VecSetSizes(x, PETSC_DECIDE, dim_cols));
+		PetscCall(VecSetFromOptions(x));
+		//PetscCall(VecSet
+		PetscReal initial_guess = 0.0;
+		PetscCall(VecSet(x, initial_guess));
 
 		/*	A COMPLETAR: Configurar solver SVD 
 		FUNCIONES:
 		SVDSetOperators
 		SVDSetFromOptions */
+		SVD svd;
+	SVDCreate(PETSC_COMM_WORLD, &svd); // Create the SVD solver context
+	SVDSetOperators(svd, A, NULL); // Set the operator matrix for the SVD solver
+	SVDSetFromOptions(svd); 
 
 		/*	A COMPLETAR: Ejecutar solver SVD
 		FUNCIONES:
 		SVDSolve */
-		
+		SVDSolve(svd);
 		/*	A COMPLETAR: Extraer datos de la solución del solver SVD
 		FUNCIONES:
 		Buscar las funciones para extraer numero de valores singulares calculados y numero de iteraciones realizadas*/
-
+	  PetscInt nsv;
+	  SVDGetConverged(svd, &nsv); 
+	  PetscPrintf(PETSC_COMM_WORLD, "Number of singular values computed: %D\n", nsv);
+	  
+	  PetscInt iterations;
+	  SVDGetIterationNumber(svd, &iterations);
+	  PetscPrintf(PETSC_COMM_WORLD, "Number of iterations: %D\n", iterations);
       /* A COMPLETAR: Bucle FOR para extraer datos de la solución del solver SVD y operación x=V*sigma^-1*Ut*b
       FUNCIONES:
       SVDGetSingularTriplet(svd, i, &sigma, u, v);
       VecDot 
       VecScale
       VecAXPBY */
+	  for (PetscInt i = 0; i < nsv; i++) {
+    PetscScalar sigma;
+    Vec u, v;
+    SVDGetSingularTriplet(svd, i, &sigma, u, v); // Get the i-th singular triplet
+
+    PetscScalar dot_product;
+    VecDot(b, u, &dot_product); // Compute dot product of b and u
+
+    PetscScalar scale_factor = 1.0 / sigma; // Compute scaling factor
+    VecScale(v, scale_factor); // Scale the vector v by the inverse of sigma
+
+    VecAXPBY(x, dot_product, 1.0, v); // Compute x = x + dot_product * v
+}
 
 		/*	A COMPLETAR: Almacenar la solucion en formato MATLAB
 		FUNCIONES:
@@ -78,14 +126,27 @@ int main(int argc, char **argv)
 		VecView(x, fdout);
 		PetscViewerPopFormat(fdout);
 		PetscViewerDestroy(&fdout);*/
+PetscViewer fdout;
+PetscViewerASCIIOpen(PETSC_COMM_WORLD, filenameS, &fdout); // Open a viewer for ASCII file
+PetscViewerPushFormat(fdout, PETSC_VIEWER_ASCII_MATLAB); // Set the output format to MATLAB
 
+// View the solution vector x
+VecView(x, fdout);
+
+PetscViewerPopFormat(fdout); // Restore the previous output format
+PetscViewerDestroy(&fdout); 
 		/*A COMPLETAR: Destruir matrices, vectores y finalizar entorno
 		FUNCIONES:
-		MatDestroy
-		VecDestroy
-      SVDDestroy
-		PetscFinalize
 		*/
+		MatDestroy(&A); // Destroy matrix A
+		VecDestroy(&b); // Destroy vector b
+		VecDestroy(&x); // Destroy vector x
+		KSPDestroy(&ksp); // Destroy the solver context
+   		PetscCall(PetscTime(&t2));
+		PetscCall(PetscPrintf(PETSC_COMM_WORLD," Elapsed Time: %f\n",t2-t1));
+
+		PetscFinalize();
+		
 
 		/*A COMPLETAR: Mostrar datos de tiempos*/
 		
