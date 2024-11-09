1d97fb69e95a1594c8f4d19eae3205225a858258
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-11-09 17:32:27 +0100

    ajustando imagen

--- PGC/application/app/config.js
+++ PGC/application/app/config.js
@@ -2,7 +2,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT || 3000,
-    DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/my-db',
-    MONGO_DB_USERNAME: process.env.MONGO_DB_USERNAME || 'root',
-    MONGO_DB_PWD: process.env.MONGO_DB_PWD || 'example',
+    DB_URL: process.env.DB_URL || 'localhost:27017',
+    MONGO_DB_USERNAME: process.env.MONGO_DB_USERNAME || 'admin',
+    MONGO_DB_PWD: process.env.MONGO_DB_PWD || 'password',
     }
