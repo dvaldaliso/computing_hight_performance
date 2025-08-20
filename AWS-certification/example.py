aa4a6b2933443c21ecd5694fceb6d2c3b3d2ff55
David Valdaliso Lopez <dvaldaliso@gmail.com>
2025-08-20 02:34:14 +0200

    elimnarcion de instancia

--- AWS-certification/example.py
+++ AWS-certification/example.py
@@ -15,10 +15,17 @@ def list_ec2_instance():
             print(f"Instance type: {instance['InstanceType']}")
             print(f"State: {instance['State']['Name']}")
             print("---------------------------")
-
+def remove_instance(instance_id):
+    ec2 = boto3.client('ec2')
+    try:
+        response = ec2.terminate_instances(InstanceIds=[instance_id])
+        print(f"Instance {instance_id} terminated successfully.")
+    except Exception as e:
+        print(f"Error terminating instance {instance_id}: {e}")
 def get_ima_user():
     iam = boto3.client('iam')
     response = iam.get_user()
     return response['User']['UserName']
 if __name__ == "__main__":
     list_ec2_instance()
+    #remove_instance('i-0af1af45ac94bfe0e')  # Replace with your instance
